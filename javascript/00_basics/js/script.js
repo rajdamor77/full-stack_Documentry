6eab987be1901c62515f1b23c098abba48d8731f
rajdamor77 <rajdamor7723@gmail.com>
2025-10-07 12:18:56 +0530

    add files

---
+++ javascript/00_basics/js/script.js
@@ -0,0 +1,86 @@
+
+console .log= "hello raj"
+
+
+//string
+let text = "We are the so-called 'Vikings' from the north.";
+
+
+//numbers 
+let mynumber= -1234.56789
+
+//Boolen 
+let mybool1 = true //1
+let mybool2 = false //0
+
+
+
+
+//null
+let mynull
+
+
+//object
+const car = {type:"Fiat", model:"500", color:"white"};
+
+//array
+let items = ["milk" , "buttermilk" , "vegetables" , "snacks"]
+let myarray = [true,123,null,"string",undefined]
+
+//hasing 
+let user2 = {
+    username : "raj123",
+    firstname: "yorichi" ,
+    age:13 ,
+    lastname:"kokushibo" ,
+    phoneno: 9687273756
+}
+
+
+//indexing ds
+let users = [{
+    firstname:"raj",
+    lastname:"kumar",
+    age:22,
+    phoneno:8799521742 ,
+    username: "mr_rk"
+} , {
+    username:"mr_jp",
+    age:25,
+    phoneno:7041521968,
+    firstname:"jaimin",
+    lastname:"patel"
+} ,
+
+]
+
+
+//undefine 
+let myunderfine = undefined
+let user = { name: "Raj" };
+console.log(user.age); // undefined (no 'age' property)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
